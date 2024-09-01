6e28107285888db6f64d109ddbc15900af8b11ca
alex <al3xback@gmail.com>
2024-09-01 18:45:36 +0700

    reformated

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ cardTitleIdEl.textContent = titleId;
 const statusItems = {
 	ethereumAmount: '0.041 ETH',
 	remainingTime: '3 days left',
-	createdDate: new Date()
+	createdDate: new Date(),
 };
 let createdDate = statusItems?.createdDate;
 console.log(createdDate);
